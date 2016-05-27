c34e38436cef7f0eb1911cf2e535f2569ba18248
oliehoek <fa.oliehoek@gmail.com>
2016-05-27 16:07:48 +0100

    adding configure script removing header replacement stuff

--- header_replacement/replace_header.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#if we want backups:
-#sed -f replace_header.sed --in-place=".backup" $@
-sed -f replace_header.sed --in-place $@
--- header_replacement/replace_headers.sh
+++
@@ -1,1 +0,0 @@
-find base/ planning/ support/ solvers/ parser/ include/ examples/ utils/ -regextype sed -regex "\(.*\.cpp\|.*\.h\)" -print0 | xargs -0 ./replace_header.sh
